1c07aceae84b38f130b25eecc94be12389cb5f9a
Eric Theise <erictheise@gmail.com>
2024-07-06 12:58:59 -0700

    Addresses https://github.com/OpenHistoricalMap/issues/issues/786 by adding a new workflow and associated bash script.

---
+++ scripts/upstream_sync.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#
+#
+#
+
+git checkout main
+git branch -D the-sacrifice
+git checkout -b the-sacrifice
+git fetch upstream master
+
+mkdir upstream
+for i in $(git ls-tree upstream/master --name-only); do
+  if [[ $i == *.md ]];then
+    git show upstream/master:$i > upstream/$i
+  fi
+done
+git show upstream/master:LICENSE > upstream/LICENSE
+git add upstream
+git commit -m 'Preserving upstream markdown & text files.'
+
+git merge --no-ff --no-commit upstream/master
+concerns=('js/ide.js' 'js/map.ts')
+for i in $(git diff --name-only --diff-filter=U --relative);do
+  echo "$i"
+  for c in "${concerns[@]}";do
+    echo "$c"
+    if [[ $i != $c ]];then
+      git checkout --theirs -- $i
+      git add $i
+    fi
+  done
+done
+
+iso=$(date -Iminutes)
+git commit -m "Merge upstream ("$iso")."
