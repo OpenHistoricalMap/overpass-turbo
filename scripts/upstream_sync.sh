be8884af5bf3bc668ff90e7f0d680a9e2f5ee450
Eric Theise <erictheise@gmail.com>
2024-07-10 20:34:46 -0700

    Part of https://github.com/OpenHistoricalMap/issues/issues/786, adjusting workflow.

--- scripts/upstream_sync.sh
+++ scripts/upstream_sync.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-if [ ! -d "upstream" ]; then
-  mkdir upstream
-fi
-for i in $(git ls-tree upstream/master --name-only); do
-  if [[ $i == *.md ]];then
-    echo $i
-    git show upstream/master:$i > upstream/$i
-  fi
-done
-git show upstream/master:LICENSE > upstream/LICENSE
-git add upstream
-if [ -n "$(git status --porcelain)" ]; then
-  git commit -m 'Preserving upstream markdown & text files.'
-fi
 
-git diff upstream/master
 
-git merge --no-ff --no-commit upstream/master
+
 concerns=('js/ide.js' 'js/map.ts')
 for i in $(git diff --name-only --diff-filter=U --relative);do
   echo "$i"
