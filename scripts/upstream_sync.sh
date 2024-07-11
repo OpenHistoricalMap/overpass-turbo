3cc55fb7812ec3e0504ab44611eaf712392b21f3
Eric Theise <erictheise@gmail.com>
2024-07-10 18:28:26 -0700

    Part of https://github.com/OpenHistoricalMap/issues/issues/786, adjusting workflow.

--- scripts/upstream_sync.sh
+++ scripts/upstream_sync.sh
@@ -21,19 +21,20 @@ fi
 git status
 
 git merge --no-ff --no-commit upstream/master
-#concerns=('js/ide.js' 'js/map.ts')
-#for i in $(git diff --name-only --diff-filter=U --relative);do
-#  echo "$i"
-#  for c in "${concerns[@]}";do
-#    echo "$c"
-#    if [[ $i != $c ]];then
-#      git checkout --theirs -- $i
-#      git add $i
-#    fi
-#  done
-#done
-#
-#if [ -n "$(git status --porcelain)" ]; then
-#  iso=$(date -Iminutes)
-#  git commit -m "Merge upstream ("$iso")."
-#fi
+concerns=('js/ide.js' 'js/map.ts')
+for i in $(git diff --name-only --diff-filter=U --relative);do
+  echo "$i"
+  for c in "${concerns[@]}";do
+    echo "$c"
+    if [[ $i != $c ]];then
+      git checkout --theirs -- $i
+      git add $i
+    fi
+  done
+done
+
+if [ -n "$(git status --porcelain)" ]; then
+  iso=$(date -Iminutes)
+  git commit -m "Merge upstream ("$iso")."
+fi
+git push origin the-sacrifice
